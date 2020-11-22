1a29910f5409594a857af911a0d731cfdc0dd121
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-11-21 16:53:34 -0800

    Cleanup and added a print function for Promises

--- src/test.js
+++ src/test.js
@@ -1,25 +1,22 @@
 const apiUrl = 'https://interview-api.sbly.com';
 const performanceEndpoint = '/ad-insights';
 const accessToken = 'SHAREABLY_SECRET_TOKEN';
-const adFetchOptions = {
-    headers: {
-        'Content-Type': 'application/json;charset=utf-8'
-    }
-};
+const adFetchOptions = { headers: { 'Content-Type': 'application/json;charset=utf-8' } };
+const dates = ['2020-01-01', '2020-01-02', '2020-01-03', '2020-01-04', '2020-01-05'];
 
-let date = '2020-01-01';
-let metrics = 'spend,impressions';
-
-getAdInsights(date, metrics).then(a => console.log(a));
-getAdBudget('bdd60126-d19a-69d2-7d10-b92955b4ee49').then(a => console.log(a));
+printResult(nextBudgets());
 
 // Helper Functions -------------------------------------------------------------------------------
+
+async function printResult(promise) {
+    promise.then(p => console.log(p));
+}
 
 function constructUrl(base, endpoint, params = {}) {
     let url = base + endpoint;
 
     let isFirst = true;
-    for (let key in params) {
+    for (key in params) {
         url += (isFirst ? '?' : '&') + key + '=' + params[key];
         isFirst = false;
     }
@@ -27,25 +24,26 @@ function constructUrl(base, endpoint, params = {}) {
     return url;
 }
 
-function getAdInsights(date, metrics) {
+async function adInsights(date, metrics) {
     let url = constructUrl(apiUrl, performanceEndpoint, {
         accessToken: accessToken,
         date: date,
         metrics: metrics
     });
 
-    return fetch(url, adFetchOptions)
+    return await fetch(url, adFetchOptions)
         .then(response => response.json())
         .catch(error => console.log(error));
 }
 
-function getAdBudget(id) {
+async function currentBudget(id) {
     let url = constructUrl(apiUrl, '/ad/' + id, {
         accessToken: accessToken
     });
 
-    return fetch(url, adFetchOptions)
+    return await fetch(url, adFetchOptions)
         .then(response => response.json())
         .then(result => result.budget)
         .catch(error => console.log(error));
+}
 }
