5300545f4dbb3d6adcbb646db32b6903e79aa6aa
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-11-21 17:00:10 -0800

    Added function to calculated weighted averages

--- src/test.js
+++ src/test.js
@@ -46,4 +46,37 @@ async function currentBudget(id) {
         .then(result => result.budget)
         .catch(error => console.log(error));
 }
+
+async function weightedAverages() {
+    let dateAdInsights = [];
+    let weightedAvgs = {};
+    let metrics = 'spend,revenue';
+
+    for (let i = 0; i < dates.length; i++) {
+        dateAdInsights.push(await adInsights(dates[i], metrics));
+    }
+
+    let numAds = dateAdInsights[0].length;
+
+    for (let i = 0; i < numAds; i++) {
+        let weightedAvg = 0;
+
+        for (let j = 0; j < dates.length; j++) {
+            let ad = dateAdInsights[j][i];
+            weightedAvg += profitMargin(ad) * ad.spend * recencyWeight(j);
+        }
+
+        let id = dateAdInsights[0][i].id;
+        weightedAvgs[id] = weightedAvg;
+    }
+
+    return weightedAvgs;
+}
+
+function profitMargin(ad) {
+    return (ad['revenue'] - ad['spend']) / ad['spend'];
+}
+
+function recencyWeight(dateIndex) {
+    return Math.pow(0.5, dates.length - 1 - dateIndex)
 }
